0bfa4d2d76e38f44c524c19451d7389371375273
Courtney Kimbrough <98043762+CourtneyKimbrough@users.noreply.github.com>
2025-04-14 05:02:20 +0000

    Initial Commit

--- app.rb
+++ app.rb
@@ -2,8 +2,59 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  choices = ["rock", "paper", "scissors"]
+  player_choice = "rock"
+  @computer_choice = choices.sample
+  
+  @outcome = ""
+
+  if @computer_choice == "rock"
+    @outcome = "tied"
+  elsif @computer_choice == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"  
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  choices = ["rock", "paper", "scissors"]
+  player_choice = "paper"
+  @computer_choice = choices.sample
+  
+  @outcome = ""
+
+  if @computer_choice == "rock"
+    @outcome = "won"
+  elsif @computer_choice == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"  
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  choices = ["rock", "paper", "scissors"]
+  player_choice = "scissors"
+  @computer_choice = choices.sample
+  
+  @outcome = ""
+
+  if @computer_choice == "rock"
+    @outcome = "lost"
+  elsif @computer_choice == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"  
+  end
+
+  erb(:scissors)
 end
